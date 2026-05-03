5fd4538d4081274e007720a9ae0208c23889cb65
Oyenike 123 <tzayn076@gmail.com>
2026-05-03 17:50:59 +0100

    test

---
+++ array.js
@@ -0,0 +1,14 @@
+let friends = ["Aisha", "Zara", "Temitope", "Ganiyat", "Maryam"]
+console.log(friends)
+
+let bestfriend = friends[1]
+console.log(bestfriend)
+
+let books = [
+    title: "My love for books",
+    description: "A love story that describes culture in Afica",
+    numberofpages: 300
+    author: "Yakubu Eniola"
+    reading: true
+];
+console.log(books);
