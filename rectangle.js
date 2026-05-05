101853e132a8c3f624bba0d20bba19d9d59da48b
Oyenike 123 <tzayn076@gmail.com>
2026-05-05 15:33:41 +0100

    Add rectangle area calculation functions

---
+++ rectangle.js
@@ -0,0 +1,19 @@
+
+function calculateAreaOfRectangle(length, width) {
+    const area = length * width;
+    console.log(area);
+}
+
+calculateAreaOfRectangle(5, 10); 
+calculateAreaOfRectangle(7, 3);
+
+
+
+function calculateAreaOfRectangle2(length, width) {
+    const area = length * width;
+    return area;
+}
+
+console.log(calculateAreaOfRectangle2(15, 20));
+console.log(calculateAreaOfRectangle2(70, 23));
+
